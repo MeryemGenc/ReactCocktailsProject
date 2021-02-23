1fd9bf3a41a5e6035f60abb82483953f97a93fcd
Meryem Gençali <merymgencc@gmail.com>
2021-02-23 19:41:39 +0300

    CocktailsProject_JohnSmilga

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi numquam
+        sit quibusdam quaerat perferendis recusandae nesciunt nobis ipsa velit
+        consequatur, perspiciatis ipsum ipsam quae quod modi deleniti, id rerum
+        similique rem obcaecati ab, doloremque deserunt! Dicta modi rem est
+        delectus assumenda dolor labore officiis minima aut esse voluptate, sed
+        distinctio magni incidunt a ex, commodi hic nulla voluptates excepturi
+        minus tenetur? Dolores cumque inventore consequuntur minima. Quae
+        dolorum neque ducimus. Labore ab rem tenetur nesciunt!
+      </p>
+    </section>
+  );
+};
+
+export default About;
